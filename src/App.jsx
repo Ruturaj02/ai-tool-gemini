4645fa73fc30f75a2ce2045b336c000ca478e7c1
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2025-06-24 01:09:42 +0530

    basic design .....

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'>
+      </div>
+      <div className='col-span-4 p-10'>
+        <div className='container h-140'>
+
+        </div>
+        <div className='bg-zinc-800 w-1/2 text-white m-auto rounded-4xl border border-zinc-700 flex h-16 p-1 pr-5'>
+          <input type="text" className='w-full h-full p-3 outline-none' placeholder='Ask me Anything' />
+          <button>Ask</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
