b5c13037c7f0eca2f173b1f8ed739f0036c606e8
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2025-06-24 01:55:28 +0530

    generate gemini api key and integrated with the application ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
+import { useState } from 'react'
 import './App.css'
+import { URL } from './constants';
 
 function App() {
+  const [question,setQuestion] = useState('');
+  const [result,setResult] = useState(undefined);
+
+
+const payload = {
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": question
+          }
+        ]
+      }
+    ]
+  }
+
+  const askQuestion =async()=>{
+    let response = await fetch(URL,{
+      method:"POST",
+      body:JSON.stringify(payload)
+
+    })
+    response = await response.json()
+
+    // console.log(response.candidates[0].content.parts[0].text);
+    setResult(response.candidates[0].content.parts[0].text)
+  }
 
   return (
     <div className='grid grid-cols-5 h-screen text-center'>
       <div className='col-span-1 bg-zinc-800'>
       </div>
       <div className='col-span-4 p-10'>
-        <div className='container h-140'>
-
+        <div className='container h-140 overflow-scroll'>
+          <div className="text-white">
+            {result}
+          </div>
         </div>
         <div className='bg-zinc-800 w-1/2 text-white m-auto rounded-4xl border border-zinc-700 flex h-16 p-1 pr-5'>
-          <input type="text" className='w-full h-full p-3 outline-none' placeholder='Ask me Anything' />
-          <button>Ask</button>
+          <input type="text" value={question} onChange={(e)=>setQuestion(e.target.value)} className='w-full h-full p-3 outline-none' placeholder='Ask me Anything' />
+          <button onClick={askQuestion}>Ask</button>
         </div>
       </div>
     </div>
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBZAAFi0mysRkYb-Dhnd_O9z7aKfNw54fg"
